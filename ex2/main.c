a39af2f6bca23f34fab6f3c073d83e520414f499
mateusgpontes <mateus.gpontes@hotmail.com>
2019-08-22 17:02:45 -0300

    add ex2

---
+++ ex2/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main (){
+    float compi = 2 * 3.1415;
+    float raio = 0;
+
+    printf("Escolha o raio para calcular o valor do comprimento da circunferência\n");
+    scanf("%f", &raio);
+    float res = compi * raio;
+    printf("O resultado foi %f\n", res);
+}
