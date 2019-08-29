abd3c83db8b4fcd158fc903e7dd3bb441220e27a
mateusgpontes <mateus.gpontes@hotmail.com>
2019-08-29 16:15:55 -0300

    create ex11

---
+++ ex11/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main (){
+    float n = 0;
+ 
+    printf("Escolha um numero: ");
+    scanf("%f", &n);
+
+    if(n > 10){
+        printf("O numero escolhido e maior que 10 \n");
+    }else if(n < 10){
+        printf("O numero escolhido e menor que 10 \n");
+    }else if(n = 10){
+        printf("O numero escolhido e igual a 10 \n");
+    }else{
+        printf("Deu ruim!");
+    }
+
+}
